5b92aa3486d27141b37494a8b7d0a4a409d3870f
mike dupont <mike.dupont@introspector.local>
2024-08-22 16:38:57 -0400

    adding flatten script

---
+++ quasi-quote-json/src/bin/flatten.rs
@@ -0,0 +1,111 @@
+extern crate serde_json;
+use serde_json::json;
+use std::env;
+use serde_json::Value;
+use std::fs::File;
+extern crate flatten_json;
+use flatten_json::flatten;
+use std::io::{self, Write};
+   
+const SCHEMA : &'static [& 'static str] = &[
+    "attr_name",
+    "attr_payload",
+    "pcd_args",
+    "pcd_attributes",
+    "pcd_name",
+    "pcd_res",
+    "pcd_vars",
+    "pexp_attributes",
+    "pexp_desc",
+    "pld_attributes",
+    "pld_mutable",
+    "pld_name",
+    "pld_type",
+    "popen_attributes",
+    "popen_expr",
+    "popen_override",
+    "psig_desc",
+    "pstr_desc",
+    "ptyp_attributes",
+    "ptyp_desc",
+//    "ptyp_loc_stack",
+    "ptype_attributes",
+    "ptype_cstrs",
+    "ptype_kind",
+    "ptype_manifest",
+    "ptype_name",
+    "ptype_params",
+    "ptype_private"
+    //    "attr_loc",
+    //    "pcd_loc",
+    //    "pexp_loc",
+    //    "pexp_loc_stack",
+    //    "pld_loc",
+    //    "popen_loc",
+    //    "psig_loc",
+    //    "pstr_loc",
+    //"pos_bol",
+    //"pos_cnum",
+    //"ptyp_loc",
+    //"ptype_loc",
+    // txt2
+    //"pos_fname"
+    //"pos_lnum"
+    //    loc2
+    // loc_end
+    // loc_ghost
+    // loc_start
+];
+
+use std::error::Error;
+
+fn top_level_search(item: &Value, depth: usize, inname: &str) -> Result<(),Box<dyn Error>>
+									//serde_json::Error,std::io::Error>
+{
+    if let Value::Array(arr) = &item {
+        for (i,item) in arr.into_iter().enumerate() {
+	    let indent = "  ".repeat(depth);
+	    let varname_item = format!("{}__{}_{}",inname,depth,i);	    
+	    println!("{} tools::find_array({},{},\"{}\",\"{}\", &[",
+		     indent,
+		     depth,
+		     i,
+		     inname,
+		     varname_item);
+	    let _ = top_level_search(item, depth + 1, &varname_item);
+	    println!("{} tools::end_array() ].to_vec()),",indent);
+	}
+    }    
+    if let Value::Object(obj) = &item {
+	for (key, value) in obj.iter() {
+	    let k =key.to_string();
+	    let k2 = k.as_str();
+	    if SCHEMA.contains(&k2) {
+		let indent = "  ".repeat(depth);
+		let parts  = k.split("_").take(2).collect::<Vec<&str>>();
+	        let outname = format!("{}__{}_{}_{}",inname,parts[0],parts[1],depth);
+		println!("{} tools::find_object({},\"{}\",\"{}\",\"{}\",\"{}\",&[",indent,depth,inname,parts[0],parts[1],&outname);
+		let _ = top_level_search(value, depth +1,&outname);
+		let mut flat_value: Value = json!({});
+		let _ = flatten(value, &mut flat_value, None, true);
+		io::stdout().write_all(serde_json::to_string(&flat_value)?.as_bytes())?;
+		
+		println!("{} tools::end_object() ].to_vec()),",indent);
+	    }
+	}
+    }
+    return Ok(());
+}
+
+fn main() {
+    for argument in env::args_os().skip(1) {
+	let json_file_path = argument;
+	let s = json_file_path.into_string().unwrap();
+	let json_data: Value = serde_json::from_reader(File::open(s).unwrap()).unwrap();
+	println!("fn main() {{");
+	println!("let results = &[");
+	let _ = top_level_search(&json_data,0,"r");
+	println!("].to_vec();");
+	println!("}}");
+    }
+}
