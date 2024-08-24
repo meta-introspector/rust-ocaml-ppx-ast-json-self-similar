3241148bbb374083d4ea7d672245661a794d5d6c
mike dupont <mike.dupont@introspector.local>
2024-08-24 10:08:25 -0400

    remove comments

--- ocaml-rust-unification/src/main.rs
+++ ocaml-rust-unification/src/main.rs
@@ -164,137 +164,6 @@ type Structure = Vec<StructureItem>;
 //#[derive(serde::Deserialize, Debug)]
 type Signature = Vec<SignatureItem>;
 
-/////////////////
-
-struct PsigDescVisitor<'de>(PhantomData<&'de ()>);
-
-impl<'de> de::Visitor<'de> for PsigDescVisitor<'de> {
-    //type Value = PsigAttribute;
-    type Value = SignatureItemDesc;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-        formatter.write_str("a psig attribute")
-    }
-
-    fn visit_seq<A>(self, mut _seq: A) -> Result<Self::Value, A::Error>
-    where
-        A: SeqAccess<'de>,
-    {
-        println!("SEQ");
-        //       let tag: String = seq.next_element()?.ok_or_else(|| de::Error::custom("expected tag"))??;
-        // match tag.as_str() {
-        //     "Psig_attribute" => {
-        //         let value: SignatureItemDesc //::PsigAttribute
-        // 	    = seq.next_element()?.ok_or_else(|| de::Error::custom("expected value"))??;
-        //         Ok(value)
-        //     }
-        //     _ => Err(de::Error::custom("unknown tag")),
-        // }
-        Err(de::Error::custom("unknown tag"))
-    }
-}
-
-impl<'de> Deserialize<'de> for SignatureItemDesc {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-	println!("DESEQ");
-        deserializer.deserialize_seq(PsigDescVisitor(PhantomData))
-    }
-}
-
-//    let psig_attribute: PsigAttribute = serde_json::from_str(json).unwrap();
-
-/////////////////
-struct SignatureItemDescVisitor;
-
-impl<'de> Visitor<'de> for SignatureItemDescVisitor {
-    type Value = SignatureItemDesc;
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("expecting a signature item descriptor")
-    }
-
-    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-    where
-        A: SeqAccess<'de>,
-    {
-        let v = seq.next_element::<serde_json::Value>();
-	match v {
-	    Ok(Some(a)) => println!("TEST seq 1 {:?}",a),
-	    Ok(None) => println!("TEST seq none"),
-	    Err(e) =>println!("err seq 1 {:?}",e),
-	}
-
-	// now lets try for the next
-	let v2 = seq.next_element::<serde_json::Value>();
-	match v2 {
-	    Ok(Some(a)) => println!("TEST seq 2 {:?}",a),
-	    Ok(None) => println!("TEST seq 2 none"),
-	    Err(e) =>println!("err seq 2 {:?}",e),
-	}
-	//println!("TEST seq {:?}",v);
-	
-        //Err(Error::invalid_type(Unexpected::Seq, &self))
-	Ok(SignatureItemDesc::PsigNone)
-    }
-
-//     fn visit_seq2<V>(self, mut visitor: V) -> Result<Self::Value, V::Error>
-//     where
-//         V: serde::de::SeqAccess<'de>,
-//     {
-// 	//let buf = String::deserialize(visitor)?;
-// 	println!("TEST INput");
-
-// 	//	let first_element = serde_json::to_string(&visitor.next_element()).expect("error serializing");
-
-// //	println!("visit_seq {:?}", first_element);	    
-//     //         .next_element::<String>()?
-//     //         .ok_or_else(|| serde::de::Error::custom("signature item descriptor empty"))?;
-
-// //	let value = match t {
-// //	    Value::String(s) => println!("visit_string {:?}", s),
-	    
-//     //         "Psig_type" => {
-//     //             let (flag, types) = data
-//     //                 .next_element::<(RecFlag, Vec<TypeDeclaration>)>()?
-//     //                 .ok_or_else(|| serde::de::Error::custom("signature item descriptor incomplete"))?;
-//     //             SignatureItemDescContent::PsigType(flag, types)
-//     //         }
-//     //         "Psig_open" => {
-//     //             let desc = data
-//     //                 .next_element::<OpenDescription>()?
-//     //                 .ok_or_else(|| serde::de::Error::custom("signature item descriptor incomplete"))?;
-//     //             SignatureItemDescContent::PsigOpen(desc)
-//     //         }
-//     //         "Psig_attribute" => {
-//     //             let attr = data
-//     //                 .next_element::<Attribute>()?
-//     //                 .ok_or_else(|| serde::de::Error::custom("signature item descriptor incomplete"))?;
-//     //             SignatureItemDescContent::PsigAttribute(attr)
-//     //         }
-//     //         _ => {
-//     //             return Err(serde::de::Error::custom(format!("signature item descriptor invalid tag: {}", t)));
-//     //         }
-// //	};
-
-// 	Ok(SignatureItemDesc::PsigNone)
-//     }
-}
-fn deserialize_signature_item<'de, D>(deserializer: D) -> Result<SignatureItemDesc, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    //    deserializer.deserialize_seq(
-    //let buf = Vec::deserialize(deserializer)?;
-    //cron::Schedule::from_str(&buf).map_err(serde::de::Error::custom)
-    
-    let foo = deserializer.deserialize_seq(SignatureItemDescVisitor {});
-    println!("TEST {:?}",foo);
-    Ok(SignatureItemDesc::PsigNone)
-    //panic!("Error occurred");
-}
-
 #[derive(serde::Deserialize, Debug)]
 struct SignatureItem {
     // psig
@@ -371,14 +240,50 @@ enum SignatureItemDesc {
     PsigNone,
 }
 
-// impl<'de> Deserialize<'de> for SignatureItemDescContent {
-//     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-//     where
-//         D: Deserializer<'de>,
-//     {
-//         deserializer.deserialize_seq(SignatureItemDescVisitor {})
-//     }
-// }
+/////////////////////
+///////////////// implementation
+
+struct SignatureItemDescVisitor;
+
+impl<'de> Visitor<'de> for SignatureItemDescVisitor {
+    type Value = SignatureItemDesc;
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("expecting a signature item descriptor")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: SeqAccess<'de>,
+    {
+        let v = seq.next_element::<serde_json::Value>();
+	match v {
+	    Ok(Some(a)) => println!("TEST seq 1 {:?}",a),
+	    Ok(None) => println!("TEST seq none"),
+	    Err(e) =>println!("err seq 1 {:?}",e),
+	}
+	// now lets try for the next
+	let v2 = seq.next_element::<serde_json::Value>();
+	match v2 {
+	    Ok(Some(a)) => println!("TEST seq 2 {:?}",a),
+	    Ok(None) => println!("TEST seq 2 none"),
+	    Err(e) =>println!("err seq 2 {:?}",e),
+	}
+	//println!("TEST seq {:?}",v);
+	
+        //Err(Error::invalid_type(Unexpected::Seq, &self))
+	Ok(SignatureItemDesc::PsigNone)
+    }
+}
+
+fn deserialize_signature_item<'de, D>(deserializer: D) -> Result<SignatureItemDesc, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let foo = deserializer.deserialize_seq(SignatureItemDescVisitor {});
+    println!("TEST {:?}",foo);
+    Ok(SignatureItemDesc::PsigNone)
+}
+
 
 fn main() {
     println!("Hello, world!");
